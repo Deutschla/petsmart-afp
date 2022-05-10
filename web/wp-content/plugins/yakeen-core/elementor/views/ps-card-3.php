d6e8add3cf698a8da7b5815b21a903063f3134d1
Neseem <info@neseem.com>
2022-05-09 17:39:16 -0700

    Card tout
    - adding responsive to card 3

--- web/wp-content/plugins/yakeen-core/elementor/views/ps-card-3.php
+++ web/wp-content/plugins/yakeen-core/elementor/views/ps-card-3.php
@@ -40,34 +40,41 @@ $main_image = Group_Control_Image_Size::get_attachment_image_html( $data, 'icon_
 	.rt-image img {
 		transform: none !important;
 	}
-	.ps-card-3 .rt-image__tout img {
-		width: 454px !important;
-		height: 454px !important;
-	}
-	.ps-card-3 .rt-image {
-		flex: 0 0 50%;
-		max-width: 50%;
-		margin-bottom: 0;
-	}
-	.ps-card-3 .rt-item{
-		display: flex;
-	}
-	.ps-card-3 .rt-item.reverse{
-		flex-direction: row-reverse
-	}
-	.ps-card-3 .rt-item.reverse .rt-image{
-		text-align: right;
-	}
+
 	.ps-card-3 .entry-content {
 		display: flex;
 		justify-content: center;
 		flex-direction: column;
 	}
+
+	
+	
+	@media (min-width: 1200px) {
+		.ps-card-3 .rt-item.reverse .rt-image{
+			text-align: right;
+		}
+		.ps-card-3 .rt-item{
+			display: flex;
+		}
+		.ps-card-3 .rt-item.reverse{
+			flex-direction: row-reverse
+		}
+		.ps-card-3 .rt-image {
+			flex: 0 0 50%;
+			max-width: 50%;
+			margin-bottom: 0;
+		}
+		.ps-card-3 .rt-image__tout img {
+			max-width: 454px !important;
+			max-height: 454px !important;
+		}
+	}
 </style>
 
 <div class="ps-card-3 hide wow fadeInUp animated" data-wow-delay="0.5s" data-wow-duration="1s">
 
-	<div class="rt-item rt-single-item <?php if ( $data['content_display'] == 'yes' ) { ?> reverse <?php } ?>">
+	<div class="rt-item rt-single-item 
+	<?php if ( $data['content_display'] == 'yes' ) {?> reverse <?php } ?>">
 		<div class="rt-image rt-image__tout">
 			<?php echo wp_kses_post( $main_image ); ?>
 		</div>
@@ -75,7 +82,6 @@ $main_image = Group_Control_Image_Size::get_attachment_image_html( $data, 'icon_
 			<h1><?php echo esc_html( $data['ps_title'] ); ?></h1>
 			<div class="post_excerpt"><p><?php echo esc_html( $data['ps_p_content'] ); ?></p></div>
 			<?php if ( ! empty( $data['button_url'] ) ) { ?>
-				<br/>
 				<div class="cta">
 					<?php echo wp_kses_post( $cta ); ?>
 				</div>
